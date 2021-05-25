1c52b13d87ab6264070f71cc46d3d4dc52cb9683
= <=>
2021-05-26 00:24:27 +0530

    update

--- android-basics-kotlin-cupcake-app-starter/app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ android-basics-kotlin-cupcake-app-starter/app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -3,6 +3,9 @@ package com.example.cupcake.model
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.Calendar
 
 class OrderViewModel : ViewModel() {
 
@@ -17,6 +20,7 @@ class OrderViewModel : ViewModel() {
 
     private val _price = MutableLiveData(0.0)
     val price: LiveData<Double> = _price
+    val dateOptions = getPickupOptions()
 
     //setters for above properties
     fun setQuantity(numbersCupcake :Int) {
@@ -31,6 +35,20 @@ class OrderViewModel : ViewModel() {
     }
     fun setDate(desiredDate:String){
         _date.value=desiredDate
+    }
+
+    // function to return the date corresponding to local date on user's device.
+    private fun getPickupOptions(): List<String> {
+        val options = mutableListOf<String>()
+        val formatter = SimpleDateFormat("E MMM d", Locale.getDefault())
+        val calender = Calendar.getInstance()
+        repeat(4){
+            options.add(formatter.format(calender.time))
+            calender.add(Calendar.DATE, 1)
+
+        }
+
+        return options
     }
 
 
