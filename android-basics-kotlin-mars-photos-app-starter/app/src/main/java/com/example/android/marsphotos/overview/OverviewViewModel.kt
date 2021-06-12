2ca041a1c7b01d732cb075267c9a323dafe4130e
hd1801 <hardikdhawan0108@gmail.com>
2021-06-12 22:26:06 +0530

    added Moshi with retrofit converter

--- android-basics-kotlin-mars-photos-app-starter/app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
+++ android-basics-kotlin-mars-photos-app-starter/app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
@@ -48,9 +48,9 @@ class OverviewViewModel : ViewModel() {
         viewModelScope.launch {
             try {
                 val listResult = MarsApiService.MarsApi.retrofitService.getPhotos()
-                _status.value = listResult
+                 _status.value = "Success: ${listResult.size} Mars photos retrieved"
             } catch (e: Exception) {
-
+                _status.value = "Failure: ${e.message}"
             }
 
         }
